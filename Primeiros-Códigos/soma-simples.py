3dc066146f895498d2d6ab4065aa5cac16189dbb
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-08-07 15:04:55 -0300

    Concatenação e soma de dois números
    Exemplos de concatenação e soma simples usando dois números

---
+++ Primeiros-Códigos/soma-simples.py
@@ -0,0 +1,9 @@
+#Esse código não faz a soma mas sim concatena os dois numeros
+n1 = input('Primeiro número ')
+n2 = input('Segundo número ')
+print('A soma é', n1+n2)
+
+#Esse código faz a soma dos dois numeros
+n1 = input('Primeiro número ')
+n2 = input('Segundo número ')
+print('A soma é', int(n1)+ int(n2))
