b2b17147148c9471a95dfa5afdf2ea98b0437c23
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-08-08 23:06:26 -0300

    Mais dois exercícios feitos

---
+++ PythonExercicios/ex014.py
@@ -0,0 +1,5 @@
+c = float(input('Escreva a temperatura em °C: '))
+f = (c*1.8)+32
+k = c-276.15
+print('A temperatura de {}°C corresponde a {:.2f}ºF'.format(c, f))
+print('A temperatura de {}°C corresponde a {:.2f}ºK'.format(c, k))
