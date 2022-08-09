fcca896dfd31f61e653a6a668b359c5c14b5c0ac
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-08-08 21:53:39 -0300

    Update ex008.py

--- PythonExercicios/ex008.py
+++ PythonExercicios/ex008.py
@@ -1,5 +1,13 @@
 m = float(input('Digite um valor em métros: '))
+km = m*0.001
+hm = m*0.01
+dam = m*0.1
+dc = m*10
 cm = m*100
 mm = m*1000
+print('{}M são {}km'.format(m, km))
+print('{}M são {}hm'.format(m, hm))
+print('{}M são {}dam'.format(m, dam))
+print('{}M são {}dc'.format(m, dc))
 print('{}M são {}cm'.format(m, cm))
 print('{}M são {}mm'.format(m, mm))
