de949971c53fe402bb455b54191f5b994bb4ca3e
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-08-10 18:45:52 -0300

    Update ex026.py

--- PythonExercicios/ex026.py
+++ PythonExercicios/ex026.py
@@ -1,4 +1,4 @@
-f = str(input('Digite uma frase: ')).strip()
-print('Nesta frase a letra "A" aparece {} vezes'.format(f.lower().count('a')))
-print('A letra "A" aparece primeiro na posição {}'.format(f.find('a')))
-print('A letra "A" aparece por ultimo na posição {}'.format(f.rfind('a')))
+f = str(input('Digite uma frase: ')).upper().strip()
+print('Nesta frase a letra "A" aparece {} vezes'.format(f.count('A')))
+print('A letra "A" aparece primeiro na posição {}'.format(f.find('A')+1))
+print('A letra "A" aparece por ultimo na posição {}'.format(f.rfind('A')+1))
