1d697003fcb59148490f40cd75ba86582451fa2c
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-08-10 18:11:24 -0300

    Update ex023.py

--- PythonExercicios/ex023.py
+++ PythonExercicios/ex023.py
@@ -1,2 +1,6 @@
-n = str(input('Digite um número de 0 a 9999: '))
-print('unidade: {}\ndezena: {}\ncentena: {}\nmilhar: {}'.format(n[3], n[2], n[1], n[0]))
+n = int(input('Digite um número de 0 a 9999: '))
+u = n // 1 % 10
+d = n // 10 % 10
+c = n // 100 % 10
+m = n // 1000 % 10
+print('unidade: {}\ndezena: {}\ncentena: {}\nmilhar: {}'.format(u, d, c, m))
