e55c07a9ab2e4f794276c82f2a088af50d665df2
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-08-16 14:59:05 -0300

    Update ex055.py

--- PythonExercicios/ex055.py
+++ PythonExercicios/ex055.py
@@ -1,9 +1,13 @@
 ma = 0
-me = 99999
+me = 0
 for i in range(0, 5):
     p = float(input('Qual o peso da {}ª pessoa? '.format(i+1)))
-    if p >= ma:
+    if i == 0:
         ma = p
-    elif p <= me:
         me = p
+    else:
+        if p > ma:
+            ma = p
+        elif p < me:
+            me = p
 print('O maior peso é {}Kg e o menor peso é {}Kg'.format(ma, me))
