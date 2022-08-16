17c70d37dd2393916db2c3840f4c47752f57181b
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-08-16 15:12:33 -0300

    Update ex056.py

--- PythonExercicios/ex056.py
+++ PythonExercicios/ex056.py
@@ -1,21 +1,21 @@
 i = 0
 mi = 0
-hmv = ''
-ihmv = 0
-m20 = 0
+nh = ''
+ih = 0
+im = 0
 for i in range(0, 4):
     print('----- {}ª PESSOA -----'.format(i+1))
-    n = str(input('Nome: '))
+    n = str(input('Nome: ')).strip()
     a = int(input('Idade: '))
-    s = str(input('Sexo [M/F]: '))
+    s = str(input('Sexo [M/F]: ')).strip()
     mi += a
     if i == 3:
         mi = mi/(i+1)
-    if s.upper() == 'M' and a >= ihmv:
-        ihmv = a
-        hmv = n
+    if s.upper() == 'M' and a >= ih:
+        ih = a
+        nh = n
     if s.upper() == 'F' and a < 20:
-        m20 += 1
+        im += 1
 print('A média de idade do grupo é de {} anos.'.format(mi))
-print('O nome do homem mais velho é {}, com {} anos.'.format(hmv, ihmv))
-print('No total {} mulheres tem menos de 20 anos de idade.'.format(m20))
+print('O nome do homem mais velho é {}, com {} anos.'.format(nh, ih))
+print('No total {} mulheres tem menos de 20 anos de idade.'.format(im))
