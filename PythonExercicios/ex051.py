cdb03296a8d30ed2186f93d896f63b15c2ab9523
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-08-16 13:59:12 -0300

    Update ex051.py

--- PythonExercicios/ex051.py
+++ PythonExercicios/ex051.py
@@ -1,3 +1,4 @@
+# Minha resolução
 n = int(input('Digite o primeiro termo da PA.: '))
 r = int(input('Digite a razão da PA.: '))
 pa = ''
@@ -11,3 +12,11 @@ for c in range(1, 11):
         n += r
         pa += str(n)+', '
 print('Os 10 primeiros termos da PA. são: \n{}'.format(pa))
+# resolução do professor
+#n = int(input('Digite o primeiro termo da PA.: '))
+#r = int(input('Digite a razão da PA.: '))
+#d = n + (10 - 1) * r
+#pa = ''
+#for c in range(n, d + r, r):
+#    print('{}'.format(c), end=' -> ')
+#print('Acabou')
