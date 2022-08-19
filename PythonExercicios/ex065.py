1b053ba5374ca25602774c0aa6d6487ce633f69d
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-08-19 18:53:26 -0300

    Update ex065.py

--- PythonExercicios/ex065.py
+++ PythonExercicios/ex065.py
@@ -4,5 +4,7 @@ while r in 'Ss':
     n = float(input('Digite um número: '))
     s += n
     c += 1
-    r = str(input('Quer continuar? [S/N]: '))
+    r = str(input('Quer continuar? [S/N]: ')).strip().upper()
+    while r not in 'SsNn':
+        r = str(input('Entrada inválida! tente novamente.\nQuer continuar? [S/N]: ')).strip().upper()
 print('A média de todos os {} números digitados é {}'.format(c, (s/c)))
