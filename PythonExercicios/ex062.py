ed5d034cdcf878383dd42b4ea051e312e03751f7
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-08-19 17:42:50 -0300

    Update ex062.py

--- PythonExercicios/ex062.py
+++ PythonExercicios/ex062.py
@@ -1,3 +1,4 @@
+# minha resolução
 t = 10
 pt = int(input('Digite o primeiro termo da PA.: '))
 r = int(input('Digeite a razão da PA.: '))
@@ -12,3 +13,22 @@ while t != 0:
         pa = ''
         pt = c + r
         ut = pt + (t - 1) * r
+# resolução do professor
+'''print('Gerador de PA')
+print('-='*10)
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print('{} -> '.format(termo), end='')
+        termo +=razão
+        cont += 1
+    print('Pausa')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finalizada com {} termos mostrados.'.format(total))
+'''
