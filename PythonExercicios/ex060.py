497fd0e27a8cc36e3e19f76a83e21b668823ad8d
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-08-19 17:01:27 -0300

    Update ex060.py

--- PythonExercicios/ex060.py
+++ PythonExercicios/ex060.py
@@ -1,11 +1,16 @@
 n = int(input('Digite um número: '))
+nf = n
 f = n
 c = n
+fat = str(c)
 # fatorial usando for
 #for n in range(n, 1, -1):
 #    f *= n-1
+#    c -= 1
+#    fat += ' x ' + str(c)
 # fatorial usando while
 while c != 1:
     f *= c-1
     c -= 1
-print('{}! = {}'.format(n, f))
+    fat += ' x '+str(c)
+print('{}!= {} = {}'.format(nf, fat, f))
