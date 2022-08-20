c74be87ce3f4291cb14b3ba8d2c53792db05d631
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-08-20 20:01:19 -0300

    Update ex067.py

--- PythonExercicios/ex067.py
+++ PythonExercicios/ex067.py
@@ -3,7 +3,7 @@ while True:
     if n < 0:
         break
     print('-='*20)
-    print(f'Tabuada do \033[33m{n}\033[m')
+    print(f'Tabuada do {n}')
     print('-=' * 20)
     for c in range(0, 10):
         print(f'{n} x {(c+1):>2} = {n*(c+1)}')
