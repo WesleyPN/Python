ebbbdc3a06280f60e95c9b2a6b38d5b421643298
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-08-22 20:38:19 -0300

    mais exercícios

---
+++ PythonTeste/aula16a.py
@@ -0,0 +1,20 @@
+"""lanche = ('Hamburger', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posição {pos}')
+
+print('Comi pra caramba!')
+print(sorted(lanche))"""
+#------------
+"""a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b + a
+print(c.count(5))
+print(c.index(5, 1))"""
+#------------
+pessoa = ('Wesley', 29, 'M', 79.9)
+#del(pessoa)
+print(pessoa)
