d64dc4733a40e66e8551a9c890d8b7a58f9be519
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-08-25 16:07:56 -0300

    Update ex072.py

--- PythonExercicios/ex072.py
+++ PythonExercicios/ex072.py
@@ -1,7 +1,16 @@
 num = (
     'Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez',
     'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Desesseis', 'Desessete', 'Desoito', 'Desenove', 'Vinte')
-n = int(input('Digite um número entre 0 e 20: '))
-while n < 0 or n > 20:
-    n = int(input('Valor inválido. Por favor, digite um número entre 0 e 20: '))
-print(f'O número {n} escrito por extenso é "{num[n]}"')
+r = 'S'
+while True:
+    n = int(input('Digite um número entre 0 e 20: '))
+    while n < 0 or n > 20:
+        n = int(input('Valor inválido. Por favor, digite um número entre 0 e 20: '))
+
+    print(f'O número {n} escrito por extenso é "{num[n]}"')
+
+    r = str(input('Quer continuar? [S/N]')).upper().strip()[0]
+    while r not in 'SsNn':
+        r = str(input('Quer continuar? [S/N]')).upper().strip()[0]
+    if r == 'N':
+        break
