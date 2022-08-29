ad9b320e4e602fce8b0f333666b91e54bd582b50
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-08-29 15:13:34 -0300

    Update ex080.py

--- PythonExercicios/ex080.py
+++ PythonExercicios/ex080.py
@@ -1,3 +1,4 @@
+#minha solução
 num = []
 for i in range(0, 5):
     n = int(input(f'Digite o {i+1}º valor: '))
@@ -9,5 +10,22 @@ for i in range(0, 5):
     else:
         num.append(n)
         print('Adicionado no final da lista...')
-
+print('-='*30)
 print(f'Os valores digitados, ordenados de forma crescente são: {num}')
+#solução do professor
+#num = []
+#for c in range(0, 5):
+#    n = int(input('Digite um valor: '))
+#    if c == 0 or n > num[-1]:
+#        num.append(n)
+#        print('Adicionado no final da lista...')
+#    else:
+#        pos = 0
+#        while pos < len(num):
+#            if n <= num[pos]:
+#                num.insert(pos, n)
+#                print(f'Adicionado na posição {pos} da lista...')
+#                break
+#            pos += 1
+#print('-='*30)
+#print(f'Os valores digitados, ordenados de forma crescente são: {num}')
