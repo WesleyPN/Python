63f7502a0dcafe9a2964759e92e71a1d5b4d6349
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-09-03 18:20:47 -0300

    Update ex085.py

--- PythonExercicios/ex085.py
+++ PythonExercicios/ex085.py
@@ -1,16 +1,12 @@
-num = []
-par = []
-imp = []
+num = [[], []]
 for c in range(0, 7):
     n = int(input(f'Digite o {c+1}º valor: '))
     if n % 2 == 0:
-        par.append(n)
-        par.sort()
+        num[0].append(n)
+        num[0].sort()
     else:
-        imp.append(n)
-        imp.sort()
-num.append(par[:])
-num.append(imp[:])
+        num[1].append(n)
+        num[1].sort()
 print('-='*30)
 print(f'Os números pares são: {num[0]}')
 print(f'Os números ímpares são: {num[1]}')
