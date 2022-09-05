1d73d1ea435809238854113177650dd54b9fbdfa
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-09-05 18:08:40 -0300

    Create aula19a.py

---
+++ PythonTeste/aula19a.py
@@ -0,0 +1,37 @@
+"""pessoas = {'nome': 'Wesley', 'sexo': 'M', 'idade': 29}
+print(f'O {pessoas["nome"]} tem {pessoas["idade"]} anos.')
+print(pessoas.keys())
+print(pessoas.values())
+print(pessoas.items())
+
+for k in pessoas.keys():
+    print(k)
+for k in pessoas.values():
+    print(k)
+for k, v in pessoas.items():
+    print(f'{k} = {v}')
+
+pessoas['nome'] = 'André'
+pessoas['peso'] = 79.8
+del pessoas['sexo']
+print(pessoas)"""
+#----------------------------
+"""brasil = []
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
+
+brasil.append(estado1)
+brasil.append(estado2)
+print(estado1)
+print(estado2)
+print(brasil[1]['uf'])"""
+#----------------------------
+estado = {}
+brasil = []
+for c in range(0, 3):
+    estado['uf'] = str(input('Unidade Federativa: '))
+    estado['sigla'] = str(input('Sigla: '))
+    brasil.append(estado.copy())
+for e in brasil:
+    for k, v in e.items():
+        print(f'O campo {k} tem valor {v}')
