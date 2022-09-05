456412e6c297009658375f598b5ba6f24351385f
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-09-05 19:39:22 -0300

    Create ex090.py

---
+++ PythonExercicios/ex090.py
@@ -0,0 +1,12 @@
+aluno = dict()
+aluno['Nome'] = str(input('Nome: ')).title()
+aluno['Média'] = float(input(f'Média de {aluno["Nome"]}: '))
+if aluno['Média'] >= 7:
+    aluno['Situação'] = 'Aprovado'
+else:
+    aluno['Situação'] = 'Reprovado'
+for k, v in aluno.items():
+    if type(v) == float:
+        print(f'{k} é igual a {v:.2f}')
+    else:
+        print(f'{k} é igual a {v}')
