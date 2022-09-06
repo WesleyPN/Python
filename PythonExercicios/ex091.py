50c2b28ec9367e33d8b61140ecaee943aec8d446
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-09-06 19:17:25 -0300

    Update ex091.py

--- PythonExercicios/ex091.py
+++ PythonExercicios/ex091.py
@@ -1,6 +1,8 @@
 from random import randint
 from time import sleep
+from operator import itemgetter
 res = dict()
+ord = dict()
 print('Valores sorteados:')
 for j in range(0, 4):
     res['Jogador-'+str(j+1)] = randint(1, 6)
@@ -11,3 +13,9 @@ print('Ranking dos jogadores:')
 for c, j in enumerate(sorted(res, key=res.get, reverse=True)):
     print(f'{c+1}ª lugar: {j} com {res[j]}')
     sleep(1)
+print('-='*30)
+ord = sorted(res.items(), key=itemgetter(1), reverse=True)
+for c, j in enumerate(ord):
+    print(f'{c+1}º lugar: {j[0]} com {j[1]}')
+    sleep(1)
+
