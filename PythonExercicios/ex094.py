e406eb9a6e8721890a75538dbef122fd3ef29e9b
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-09-06 20:35:15 -0300

    Update ex094.py

--- PythonExercicios/ex094.py
+++ PythonExercicios/ex094.py
@@ -2,24 +2,24 @@ pessoa = {}
 listaP = []
 listaM = []
 listaAM = []
-cont = c = 0
+cont = 0
 while True:
     pessoa['Nome'] = str(input('Nome: '))
     pessoa['Idade'] = int(input('Idade: '))
-    pessoa['Sexo'] = str(input('Sexo [M/F]: ')).upper().strip()[0]
-    while pessoa['Sexo'] not in 'MF':
+    while True:
         pessoa['Sexo'] = str(input('Sexo [M/F]: ')).upper().strip()[0]
+        if pessoa['Sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.')
     listaP.append(pessoa.copy())
     cont += pessoa['Idade']
-    c += 1
-
-    r = str(input('Quer continuar? [S/N]')).upper().strip()[0]
-    while r not in 'SN':
+    while True:
         r = str(input('Quer continuar? [S/N]')).upper().strip()[0]
+        if r in 'SN':
+            break
+        print('ERRO! Por favor, digite apenas S ou N.')
     if r == 'N':
-        print('-='*30)
         break
-
 print(f'Foram cadastradas {len(listaP)} pessoas.')
 print(f'A média de idade do grupo é de {cont/len(listaP):.2f} anos.')
 print('As mulheres cadastradas são: ', end='')
@@ -27,9 +27,11 @@ for v in listaP:
     if v['Sexo'] == 'F':
         print(f'{v["Nome"]} ', end='')
 print()
-print('Lista das pessoas com idade acima da média:')
+print('Lista das pessoas com idade acima da média:', end='')
 for v in listaP:
-    for k, n in v.items():
-        if v['Idade'] > cont/len(listaP):
+    if v['Idade'] > cont / len(listaP):
+        print('     ')
+        for k, n in v.items():
             print(f'{k} = {n}', end='; ')
-    print()
+        print()
+print('<< ENCERRADO >>')
