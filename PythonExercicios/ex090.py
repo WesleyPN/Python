df9d01e69a4837fe4e98cebce7f8d72b0df01e12
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-09-06 17:02:54 -0300

    Update ex090.py

--- PythonExercicios/ex090.py
+++ PythonExercicios/ex090.py
@@ -3,6 +3,8 @@ aluno['Nome'] = str(input('Nome: ')).title()
 aluno['Média'] = float(input(f'Média de {aluno["Nome"]}: '))
 if aluno['Média'] >= 7:
     aluno['Situação'] = 'Aprovado'
+elif 5 < aluno['Média'] < 7:
+    aluno['Situação'] = 'Recuperação'
 else:
     aluno['Situação'] = 'Reprovado'
 for k, v in aluno.items():
