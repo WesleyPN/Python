22e820d4a8c26826e336df66a9689b43a74db263
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-09-06 19:32:41 -0300

    Update ex092.py

--- PythonExercicios/ex092.py
+++ PythonExercicios/ex092.py
@@ -6,6 +6,6 @@ apo['CTPS'] = int(input('Carteira de Trabalho (0 não tem): '))
 if apo['CTPS'] != 0:
     apo['AnoCont'] = int(input('Ano de contratação: '))
     apo['Salário'] = float(input('Salário: '))
-    apo['AnoApo'] = date.today().year - apo['AnoCont']+35
+    apo['IdadeApo'] = apo['Idade'] + ((apo['AnoCont']+35) - date.today().year)
 for k, v in apo.items():
     print(f'{k} tem o valor {v}')
