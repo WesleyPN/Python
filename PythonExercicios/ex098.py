0bac3bc5854167a1a1efeae4ebfcb61a577b9ab8
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-09-10 17:30:42 -0300

    Create ex098.py

---
+++ PythonExercicios/ex098.py
@@ -0,0 +1,54 @@
+from time import sleep
+
+
+def contador(inicio, fim, passo):
+    if inicio < fim and passo > 0:
+        print(f'Contagem de {inicio} até {fim} de {passo} em {passo}:')
+        for n in range(inicio, (fim+1), passo):
+            print(n, end=' ')
+            sleep(0.5)
+        print()
+    elif inicio < fim and passo == 0:
+        passo += 1
+        print(f'Contagem de {inicio} até {fim} de {passo} em {passo}:')
+        for n in range(inicio, (fim+1), passo):
+            print(n, end=' ')
+            sleep(0.5)
+        print()
+    elif inicio < fim and passo < 0:
+        print(f'Contagem de {inicio} até {fim} de {passo*-1} em {passo*-1}:')
+        for n in range(inicio, (fim+1), (passo*-1)):
+            print(n, end=' ')
+            sleep(0.5)
+        print()
+
+    elif inicio > fim and passo > 0:
+        print(f'Contagem de {inicio} até {fim} de {passo} em {passo}:')
+        for n in range(inicio, (fim-1), (passo*-1)):
+            print(n, end=' ')
+            sleep(0.5)
+        print()
+    elif inicio > fim and passo == 0:
+        passo += 1
+        print(f'Contagem de {inicio} até {fim} de {passo} em {passo}:')
+        for n in range(inicio, (fim-1), (passo*-1)):
+            print(n, end=' ')
+            sleep(0.5)
+        print()
+    elif inicio > fim and passo < 0:
+        print(f'Contagem de {inicio} até {fim} de {passo*-1} em {passo*-1}:')
+        for n in range(inicio, (fim-1), passo):
+            print(n, end=' ')
+            sleep(0.5)
+        print()
+
+
+contador(1, 10, 1)
+print('-='*30)
+contador(10, 0, 2)
+print('-='*30)
+i = int(input('Digite o início da contagem: '))
+f = int(input('Digite o fim da contagem: '))
+p = int(input('Digite o passo da contagem: '))
+print('-='*30)
+contador(i, f, p)
