50e0fd4154e240b6b9d45fe60306e7c86866cc90
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-09-10 17:53:09 -0300

    Create ex099.py

---
+++ PythonExercicios/ex099.py
@@ -0,0 +1,27 @@
+from time import sleep
+
+
+def maior(*num):
+    maiorNumero = 0
+    print('Analizando os valores passados...')
+    for c, n in enumerate(num):
+        print(n, end=' ')
+        sleep(0.5)
+        if c == 0:
+            maiorNumero = n
+        elif n >= maiorNumero:
+            maiorNumero = n
+    print(f'Foram informados {len(num)} valores ao todo.')
+    print(f'O maior número é {maiorNumero}')
+
+
+print('-='*30)
+maior(2, 9, 4, 5, 7, 1)
+print('-='*30)
+maior(4, 7, 2)
+print('-='*30)
+maior(1, 2)
+print('-='*30)
+maior(6)
+print('-='*30)
+maior()
