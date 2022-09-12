99488deac70c0d335283fade316e7979977063a4
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-09-12 18:24:46 -0300

    Update ex100.py

--- PythonExercicios/ex100.py
+++ PythonExercicios/ex100.py
@@ -6,7 +6,7 @@ def sorteia(lst):
     print('Sorteando 5 valores da lista:', end=' ')
     for n in range(0, 5):
         lst.append(randint(1, 10))
-        print(lst[n], end=' ')
+        print(lst[n], end=' ', flush=True)
         sleep(0.3)
     print('PRONTO!')
 
@@ -14,8 +14,8 @@ def sorteia(lst):
 def somaPar(lst):
     soma = 0
     print(f'Somando os valores pares de {lst},', end=' ')
-    for c, v in enumerate(lst):
-        if lst[c] % 2 == 0:
+    for v in lst:
+        if v % 2 == 0:
             soma += v
     print(f'temos {soma}')
 
