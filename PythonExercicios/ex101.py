1a92abeee8ee7ff234e9a260675ca4697c1973b1
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-09-13 17:10:47 -0300

    Update ex101.py

--- PythonExercicios/ex101.py
+++ PythonExercicios/ex101.py
@@ -1,10 +1,9 @@
-from datetime import date
-
-
 def voto(anoNascimento=0):
+    from datetime import date
+    
     anoAtual = date.today().year
     idade = anoAtual - anoNascimento
-    if idade < 18:
+    if idade < 16:
         return f'Com {idade} anos: NÂO VOTA.'
     elif 18 <= idade < 65:
         return f'Com {idade} anos: VOTO OBRIGATÓRIO.'
