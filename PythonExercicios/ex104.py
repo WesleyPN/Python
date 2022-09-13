518a55e2ccf113fb13b5bbfd80af0266da692fed
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-09-13 17:54:15 -0300

    Update ex104.py

--- PythonExercicios/ex104.py
+++ PythonExercicios/ex104.py
@@ -1,11 +1,16 @@
 def leiaInt(txt=''):
+    cond = False
+    val = 0
     while True:
         num = str(input(txt)).strip()
         if num.isnumeric():
-            return num
+            val = int(num)
+            cond = True
         else:
             print('\033[31mERRO! Digite um número inteiro válido.\033[m')
-
+        if cond:
+            break
+    return num
 
 n = leiaInt('Digite um número: ')
 print(f'Você acabou de digitar o número {n}')
