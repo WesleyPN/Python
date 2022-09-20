3de2358299ff162f3ff72f9da6e4b83baea2291c
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-09-20 01:09:28 -0300

    ex107

---
+++ PythonExercicios/ex107/moeda.py
@@ -0,0 +1,16 @@
+def aumentar(n, p):
+    porcento = n * p / 100
+    return n + porcento
+
+
+def diminuir(n, p):
+    porcento = n * p / 100
+    return n - porcento
+
+
+def dobro(n):
+    return n * 2
+
+
+def metade(n):
+    return n / 2
