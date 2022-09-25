1a5e06614920de49ff6033b4aba355cf5b5fa7b7
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-09-25 13:39:48 -0300

    Update ex114.py

--- PythonExercicios/ex114.py
+++ PythonExercicios/ex114.py
@@ -1,8 +1,7 @@
-import urllib
 from urllib import request, error
 try:
-    site = urllib.request.urlopen('http://www.pudim.com.br')
-except urllib.error.URLError:
+    site = request.urlopen('http://www.pudim.com.br')
+except error.URLError:
     print('\033[31mO site Pudim não está acessível no momento.\033[m')
 else:
     print('\033[32mConsegui acessar o site Pudim com sucesso!\033[m')
