86e00281d603b08d7588463d9f67273c291a7e95
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-11-21 13:35:18 -0300

    Create Fliper.py

---
+++ Exercícios/Fliper.py
@@ -0,0 +1,36 @@
+"""
+Flíper
+
+Flíper é um tipo de jogo onde uma bolinha de metal cai por um labirinto de caminhos 
+até chegar na parte de baixo do labirinto. A quantidade de pontos que o jogador ganha depende 
+do caminho que a bolinha seguir. O jogador pode controlar o percurso da bolinha mudando a posição 
+de algumas portinhas do labirinto. Cada portinha pode estar na posição 0, que significa virada 
+para a esquerda, ou na posição 1 que quer dizer virada para a direita. Considere o flíper da 
+figura abaixo, que tem duas portinhas. A portinha P está na posição 1 e a portinha R, na posição 0. 
+Desse jeito, a bolinha vai cair pelo caminho B.
+
+Figura 1 = https://api.neps.academy/image/667.png
+
+Você deve escrever um programa que, dadas as posições das portinhas P e R, neste flíper da figura, 
+diga por qual dos três caminhos, A, B ou C, a bolinha vai cair!
+Entrada
+
+A entrada é composta por apenas uma linha contendo dois números PP e RR, indicando as posições das 
+duas portinhas do flíper da figura.
+Saída
+
+A saída do seu programa deve ser também apenas uma linha, contendo uma letra maiúscula que indica 
+o caminho por onde a bolinha vai cair: 'A', 'B' ou 'C'.
+Restrições
+
+    O número P pode ser 0 ou 1. O número R pode ser 0 ou 1.
+"""
+P = int(input())
+R = int(input())
+if P == R == 1:
+    print("A")
+elif P == 1 and R == 0:
+    print("B")
+elif P == R == 0 or P != R == 1:
+    print("C")
+    
